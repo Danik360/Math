ef1b09d580c609ba97450584a43e2a3a3b5ba367
Danik360 <mrdsmrdsaty@gmail.com>
2026-02-05 19:31:11 +0500

    доделал текст над едой

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,12 +6,14 @@ public class GameManager : MonoBehaviour
     public Text[] AllText;
     public static GameManager Instance;
     [SerializeField] public FoodSpawner FoodSp;
+    private int Usetext;
+    private int Answer;
+    private int falseAnswer1;
+    private int falseAnswer2;
 
     [Header("UI")]
     public Text Text;
     public GameObject gameOverPanel;
-
-    private int score = 0;
 
     void Awake()
     {
@@ -21,6 +23,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         gameOverPanel.SetActive(false);
+        Usetext = 0;
     }
 
     public void GameOver()
@@ -37,17 +40,65 @@ public class GameManager : MonoBehaviour
 
     public void TextPosition(Vector2 Pos)
     {
-        for (int i = 0; i < 4; i++)
+        if (Usetext == 0)
         {
-            
+            Debug.Log("сработал текст номер 1");
+            AllText[Usetext].text = Answer.ToString();
+            AllText[Usetext].transform.position = Pos;
+            Usetext++;
         }
+        else if (Usetext == 1)
+        {
+            Debug.Log("сработал текст номер 2");
+            AllText[Usetext].text = falseAnswer1.ToString();
+            AllText[Usetext].transform.position = Pos;
+            Usetext++;
+        }
+        else
+        {
+            Debug.Log("сработал текст номер 3");
+            AllText[Usetext].text = falseAnswer2.ToString();
+            AllText[Usetext].transform.position = Pos;
+        }
+        Debug.Log($"Текст {Usetext} позиция: {Pos}, активен: {AllText[Usetext].gameObject.activeSelf}");
+        AllText[Usetext].transform.position = Pos;
+        AllText[Usetext].gameObject.SetActive(true);  // принудительно включи
+
     }
 
-    public void MathExs()
+        public void MathExs()
     {
+        int Numex1 = Random.Range(2,10);
+        int Numex2 = Random.Range(2,10);
         int Num1 = Random.Range(2, 20);
         int Num2 = Random.Range(2, 20);
-        Text.text = Num1 + " + " + Num2 + "= ?";
-        int Answer = Num1 + Num2;
+        int Type = Random.Range(1, 3);  // 1-2, а не 1-2 (было 1,2 одинаково)
+
+        if (Type == 1)
+        {
+            Text.text = Num1 + " + " + Num2 + "= ?";
+            Answer = Num1 + Num2;              // убрать int (локальную)
+            falseAnswer1 = Num1 + Num2 + Numex1;
+            falseAnswer2 = Num1 + Num2 - Numex2;
+        }
+        else  // Type == 2
+        {
+            if (Num1 >= Num2)
+            {
+                Text.text = Num1 + " - " + Num2 + "= ?";
+                Answer = Num1 - Num2;
+                falseAnswer1 = Num1 - Num2 + Numex1;
+                falseAnswer2 = Num1 - Num2 - Numex2;
+            }
+            else
+            {
+                Text.text = Num2 + " - " + Num1 + "= ?";
+                Answer = Num2 - Num1;
+                falseAnswer1 = Num2 - Num1 + Numex1;
+                falseAnswer2 = Num2 - Num1 - Numex2;
+            }
+        }
+        
+        Debug.Log($"Правильный: {Answer}, Ложные: {falseAnswer1}, {falseAnswer2}");  // для проверки
     }
 }
--- Assets/SpawnFood.cs
+++ Assets/SpawnFood.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;  // для Canvas
 
 public class FoodSpawner : MonoBehaviour
 {
-    [SerializeField]public GameManager GM;
+    [SerializeField] public GameManager GM;
     public GameObject foodPrefab;
     public Vector2 mapSize = new Vector2(20, 20);
+    
+    [SerializeField] private Canvas uiCanvas;  // перетащи Canvas в инспекторе
+    [SerializeField] private Camera mainCamera;  // главная камера
 
     void Start()
     {
-        for (int i = 0; i < 4; i++)
+        GM.MathExs();
+        
+        for (int i = 0; i < 3; i++)
         {
-            GM.MathExs();
-            Vector2 randomPos = new Vector2
-            (
-                Mathf.Round(Random.Range(-mapSize.x / 2, mapSize.x / 2)),
-                Mathf.Round(Random.Range(-mapSize.y / 2, mapSize.y / 2))
-            );
-            Instantiate(foodPrefab, randomPos, Quaternion.identity);
+            // Инициализируем сразу полностью!
+            Vector2 worldPos = Vector2.zero;  // или new Vector2(0,0)
             
+            worldPos.x = Mathf.Round(Random.Range(-mapSize.x / 2, mapSize.x / 2));
+            worldPos.y = Mathf.Round(Random.Range(-mapSize.y / 2, mapSize.y / 2));
+            
+            // Теперь используем
+            Instantiate(foodPrefab, worldPos, Quaternion.identity);
+            GM.TextPosition(worldPos);
         }
     }
 }
